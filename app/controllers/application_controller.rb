fd23e9ca75289a1c79598f9eee84684c761a375b
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-04-27 11:23:00 -0700

    commit tree 876a4fddf47405b1c1df8d7d039989dcf376df32

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,39 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+ 
 
-  # add routes
+  get '/' do
+    'Hello World! its baking time!'
+  end
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end 
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get "/baked_goods" do 
+    baked_goods = BakedGood.all
+    baked_goods.to_json
+  end
+
+  get "/baked_goods/by_price" do
+    baked_goods = BakedGood.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  # get '/baked_goods/:id' do
+  #   baked_good = BakedGood.find(params[:id])
+  #   baked_good.to_json(include: {bakery: {only: :name}})
+  # end
+
+  get '/baked_goods/most_expensive' do
+    baked_good = BakedGood.order(:price).last
+    baked_good.to_json
+  end
 
 end
